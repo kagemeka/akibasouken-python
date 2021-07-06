1586f499e8a513a7078bdfbe0ee90fc8cd14ab1c
kagemeka <kagemeka1@gmail.com>
2021-07-06 12:31:55 +0900

    enhance: create docker environment for lambda (#59)

--- build_lambda.sh
+++
@@ -1,8 +0,0 @@
-mkdir lambda_env/
-cp -r src/* lambda_env/
-pip install -r env/py/requirements.txt -t lambda_env/
-cd lambda_env/
-zip -r lambda_scrape_akibasouken.zip ./*
-mv lambda_scrape_akibasouken.zip ../
-cd ..
-rm -r lambda_env/
